9683f694d74f3bc76f1ad4d9fb6367bb561d4507
Yisel0911 <90224925+Yisel0911@users.noreply.github.com>
2021-09-23 18:44:51 -0500

    Update CtrlUsuarios.java
    Controller Usuario

--- BackendRockola/src/main/java/controller/CtrlUsuarios.java
+++ BackendRockola/src/main/java/controller/CtrlUsuarios.java
@@ -1,7 +1,32 @@
 package controller;
 
+
+import DAO.DAOUsuarios;
+import TO.TOusuario;
+import java.util.ArrayList;
+
 public class CtrlUsuarios {
-    
+
     DAOUsuarios usuariosDAO;
+
+    public CtrlUsuarios() {
+        this.usuariosDAO = new DAOUsuarios();
+    }
+
+    public ArrayList<TO.TOusuario> consultarUsuarios() {
+        return usuariosDAO.consultarUsuarios();
+    }
+
+    public int inserarUsuarios(TO.TOusuario tOUsuarios) {
+        return usuariosDAO.insertarUsuarios(tOUsuarios);
+    }
+
+    public boolean actualizarUsuarios(TO.TOusuario tOUsuarios) {
+        return usuariosDAO.actualizarUsuarios(tOUsuarios);
+    }
+
+    public boolean eliminarUsuarios(int idUsuarios) {
+        return usuariosDAO.eliminarUsuarios(idUsuarios);
+    }
 
 }
