9c654f2a7ed78a23c3b245b0ee73eb2ca97f35f3
Yisel0911 <90224925+Yisel0911@users.noreply.github.com>
2021-09-22 21:15:34 -0500

    Update DAOUsuarios.java
    DAOUsuarios

--- BackendRockola/src/main/java/DAO/DAOUsuarios.java
+++ BackendRockola/src/main/java/DAO/DAOUsuarios.java
@@ -1,14 +1,21 @@
 package DAO;
 
 import java.util.ArrayList;
-
+import TO.TOusuario;
 import db.ConexionDB;
 
 public class DAOUsuarios {
 	
-	private ConexionDB con = new ConexionDB();
-	private String nombreTabla = "Usuarios";
-	
-	private ArrayList usuario = new ArrayList();
+    private ConexionDB con = new ConexionDB();
+
+    public DAOUsuarios() {
+    }
+        
+     public ArrayList<TOusuario> consultarUsuarios(){
+         ArrayList<TOusuario> usuario = new ArrayList<>();
+         con.consultar("Usuarios");
+         return null;
+     }   
+        
 	
 }
